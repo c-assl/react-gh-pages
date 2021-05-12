c6b6b46d99086e5765c62d0d9709f6ac274d412d
cecile <cecileasselin@MacBook-Pro-de-Amelie.local>
2021-05-12 13:53:39 +0200

    try to import svg image as component

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -4,8 +4,13 @@ import React from 'react';
 import Content from '!babel-loader!@mdx-js/loader!./content.mdx';
 
 function Home() {
-  return <h2>Retour à bon port</h2>;
-  <Content/>
+  return (
+    <div>
+      <h2>Retour à bon port</h2>
+      <Content />
+    </div>
+  )
 }
 
+// <img src={'../../../public/svg/Ports de la région Poitou, Aunis, Saintonge en 1789.svg'} />
 export default Home;
